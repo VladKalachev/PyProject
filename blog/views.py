7cbd908ff5786bf19ec7b97a1ab55c9e412552a3
Vlad Kalashev <vlvete@inbox.ru>
2016-03-13 20:01:52 +0300

    v0.1

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,8 @@ def index(request):
 
 
 def current_datetime(request):
+    name = "Vlad"
     now = datetime.datetime.now()
     t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now}))
+    html = t.render(Context({'current_date': now, 'name': name}))
     return HttpResponse(html)
