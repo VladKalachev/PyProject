bdfdcebb213fd32e27dbfa16c1b508849a6dffc0
Vlad Kalashev <vlvete@inbox.ru>
2016-03-13 19:18:12 +0300

    two commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.template.loader import get_template
+from django.template import Context
 
 # Create your views here.
 import datetime
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def hello(request):
     return HttpResponse("Hello world")
@@ -11,8 +14,8 @@ def index(request):
     return HttpResponse("Hove a you?")
 
 
-
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = '<html><body>Сейчас %s.</body></html>' % now
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'current_date': now}))
     return HttpResponse(html)
--- job/settings.py
+++ job/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'job.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:/Users/Vlad/Desktop/pythone/job/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
